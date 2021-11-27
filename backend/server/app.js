f642e8bd6c921369aa88279d8a80da91f685627e
SzymonNowak <nnja5@o2.pl>
2021-11-27 20:18:06 +0100

    connect with db work

--- backend/server/app.js
+++ backend/server/app.js
@@ -2,23 +2,34 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 const dotenv = require('dotenv').config();
 // import routes
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 
 const app = express();
-const PORT = 5000;
-const HOST = '0.0.0.0';
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${process.env.PORT}`);
+// app.use(bodyParser.json({ limit: '30mb', extends: true }));
+// app.use(bodyParser.urlencoded({ limit: '30mb', extends: true }));
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kz7da.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  )
+  .then(() => app.listen(PORT, () => console.log(`app runing on ${PORT}`)))
+  .catch(error => console.log(error));
 export default app;
